c2e11a3f6de3dd95ce2f09efbb17bd1fc2a2b4fd
Luca <avilaluca61@gmail.com>
2025-02-14 16:41:05 -0300

    Adjust size of the container div

--- script.js
+++ script.js
@@ -3,4 +3,12 @@ const container = document.querySelector(".container");
 for (let i = 0; i < 256; i++) {
   const div = document.createElement("div");
   container.appendChild(div);
+  div.classList.add("hoverEffect");
 }
+
+const hoverEff = document.querySelectorAll(".hoverEffect");
+hoverEff.forEach((div) => {
+  div.addEventListener("mouseover", () => {
+    div.style.backgroundColor = "black";
+  });
+});
